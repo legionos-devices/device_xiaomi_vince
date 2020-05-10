ec01ca5374caf91d6fb1c7031c5754141ee87c77
rajkale99 <kaleraj.rk@gmail.com>
2020-05-10 13:09:37 +0530

    Update fingerprint from coral May 2020 patch

--- legion_vince.mk
+++ legion_vince.mk
@@ -39,7 +39,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="vince-user 8.1.0 OPM1.171019.019 V10.0.4.0.OEGMIFH release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "xiaomi/vince/vince:8.1.0/OPM1.171019.019/V10.0.4.0.OEGMIFH:user/release-keys"
+BUILD_FINGERPRINT := "google/coral/coral:10/QQ2A.200501.001.A3/6353761:user/release-keys"
 
 #Official
 LEGION_BUILD_TYPE := OFFICIAL
