f6d4449ce2beab6a87ecfbea841e6f045fa395f4
Tortel <tortel1210@gmail.com>
2020-07-09 21:23:32 +0000

    Build btconfigstore from source

--- device.mk
+++ device.mk
@@ -136,6 +136,12 @@ PRODUCT_PACKAGES += \
     libldacBT_dec \
     libbt-vendor
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.btconfigstore@1.0.vendor
+
+
+
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
